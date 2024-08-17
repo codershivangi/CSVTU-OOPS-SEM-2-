b951b8190b2b29e6fa8438cbd7448631a914c178
Shivangi <159949628+codershivangi@users.noreply.github.com>
2024-08-17 10:03:36 -0700

    Add files via upload

---
+++ Pattern-printing/Program to print a rectangle pattern.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    return 0;
+    
+}
